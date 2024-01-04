0a75408c8a351c8ec9659c8da8aa6ca800b67473
sakagami <seiroka@outlook.com>
2024-01-04 23:41:22 +0800

    audio display rollback point

--- src/app/audio/page.tsx
+++ src/app/audio/page.tsx
@@ -157,9 +157,46 @@ export default function Audio() {
 
   return (
     <div className="flex flex-col">
-      <div>
+      <div className="pb-3">
         <Navbar />
       </div>
+
+      <div
+        className={`flex flex-col rounded-lg overflow-hidden border-2 border-dashed border-gray-300 ${
+          isMediaDragging ? "border-blue-500" : ""
+        } ${mediaSource ? "border-none" : ""}`}
+        onDrop={handleMediaDrop}
+        onDragOver={handleMediaDragOver}
+        onDragLeave={handleMediaDragLeave}
+      >
+        {/* Audio player area */}
+        {mediaSource && (
+          <audio controls autoPlay={isPlaying} className="w-full">
+            <source src={mediaSource} type="audio/mp3" />
+            <source src={mediaSource} type="audio/m4a" />
+            Your browser does not support the audio element.
+          </audio>
+        )}
+
+        {/* Drag-and-drop prompt */}
+        {!mediaSource && (
+          <div className="text-center p-4">
+            <p>Drag and drop a video file here to play.</p>
+          </div>
+        )}
+      </div>
+
+      <div className="p-3">
+        {" "}
+        <div className="flex flex-col p-7 rounded-lg overflow-hidden border-2 border-amber-700 bg-white">
+          {subtitles.map((subtitle, index) => (
+            <p className="" key={index}>
+              {subtitle.text}
+            </p>
+          ))}
+        </div>
+      </div>
+
       <div className="flex flex-row justify-center">
         <div className="basis-3/5 p-5">
           <div
